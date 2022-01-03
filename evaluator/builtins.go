3db0c58dd4816e24dcf46c63f476d3318ed8699f
Sudipto <sudipto00003@gmail.com>
2022-01-03 17:34:00 +0600

    print function added #success

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,8 @@
 package evaluator
 
 import (
+	"fmt"
+
 	"github.com/sudipto-003/dyno/object"
 )
 
@@ -94,6 +96,15 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
